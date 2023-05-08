027480b9e0cc320acfbb599ec8a00cf8e14cc8ed
Danny <29934021+appliquant@users.noreply.github.com>
2023-05-08 21:13:52 +0000

    Créer un composant des sons

--- projet_musique/frontend/vue-project/src/views/OtherCreationsView.vue
+++ projet_musique/frontend/vue-project/src/views/OtherCreationsView.vue
@@ -1,79 +1,26 @@
 <script setup lang="ts">
-import { onMounted, ref, computed } from 'vue'
-import WaveSurfer from 'wavesurfer.js'
-
+import SoundCard from '@/components/MusicCard.vue'
 import sound from '../assets/audio/demo.wav'
-import IconPlay from '@/components/icons/IconPlay.vue'
-import IconStop from '@/components/icons/IconStop.vue'
-
-let wavesurfer: WaveSurfer | null = null
-
-const isPlaying = ref(false)
-const duration = ref(0)
-
-const handlePlayingAudio = () => {
-  if (!wavesurfer) return
-
-  if (isPlaying.value === false) {
-    isPlaying.value = true
-    return wavesurfer.play()
-  } else {
-    isPlaying.value = false
-    return wavesurfer.pause()
-  }
-}
-
-onMounted(() => {
-  wavesurfer = WaveSurfer.create({
-    container: '#waveform',
-    scrollParent: false
-  })
-
-  const audio = new Audio(sound)
-
-  wavesurfer.load(audio)
-  wavesurfer.on('ready', () => {
-    duration.value = Math.round(wavesurfer?.getDuration() ?? 0)
-  })
-})
 </script>
 
 <template>
   <main>
     <h1>Créations des autres utilisateurs</h1>
 
-    <div class="music-card">
-      <div class="music-card__header">
-        <div>
-          <IconPlay v-if="!isPlaying" class="icon" @click="handlePlayingAudio" />
-          <IconStop v-if="isPlaying" class="icon" @click="handlePlayingAudio" />
-        </div>
-        <div>
-          <h1>Création #1</h1>
-          <p class="music-card__author">bob dyx ● {{ duration }}s</p>
-        </div>
-      </div>
-      <div id="waveform"></div>
-    </div>
+    <SoundCard :path-to-audio="sound" />
   </main>
 </template>
 
 <style scoped>
 main {
   margin: 5em;
-  /* display: flex; */
-  /* flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  position: relative; */
 }
 
 main > h1 {
   margin-bottom: 2em;
 }
 
-.music-card {
+/* .music-card {
   border: 0.03em solid var(--tertiary-color);
   border-radius: 0.5em;
   padding: 1em;
@@ -88,5 +35,5 @@ main > h1 {
 .music-card__author {
   font-weight: 300;
   font-size: 1em;
-}
+} */
 </style>
