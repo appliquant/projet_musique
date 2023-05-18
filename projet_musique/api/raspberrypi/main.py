37660f7e688b2ce3ac0a52601532083512c941e6
Danny <29934021+appliquant@users.noreply.github.com>
2023-05-18 20:32:14 +0000

    code sur github

--- projet_musique/api/raspberrypi/main.py
+++ projet_musique/api/raspberrypi/main.py
@@ -1,133 +1,86 @@
-"""
-Jouer sons avec boutons et LEDs
-"""
-from time import sleep
+# -*- coding: utf-8 -*-
+
 import pygame
+from time import sleep
 from gpiozero import LED, Button, MCP3008
-# from pydub import AudioSegment
 
-# LEDs et boutons
-led_red = LED(17)         # gpio17
-led_yellow = LED(27)      # gpio27
-led_green = LED(22)       # gpio22
+led_one = LED(24)         # gpio17
+led_two = LED(25)      # gpio27
+led_three = LED(26)       # gpio22
+led_four = LED(16)
 
-button_red = Button(2)    # gpio2
-button_yellow = Button(3)  # gpio3
-button_green = Button(4)  # gpio4
+button_one = Button(23)    # gpio2
+button_two = Button(22)  # gpio3
+button_three = Button(27)  # gpio4
+button_four = Button(17)
 
 potentiometer = MCP3008(channel=0)
 
-# Initialisation de pygame
-# Voir https://projects.raspberrypi.org/en/projects/gpio-music-box/1
 pygame.mixer.init()
-# sound_red = pygame.mixer.Sound("chemin/vers/son/rouge.wav")
-# sound_yellow = pygame.mixer.Sound("chemin/vers/son/jaune.wav")
-# sound_green = pygame.mixer.Sound("chemin/vers/son/vert.wav")
+sound = pygame.mixer.Sound("snare5.wav")  # kick5.wav - snare5.wav
 
 
-# Fonctions pour allumer les LEDs
-def toggle_red():
-    """
-    Allume la LED rouge
-    """
-    led_red.toggle()
+def toggle_led_one():
+    led_one.toggle()
 
 
-def toggle_yellow():
-    """
-    Allume la LED jaune
-    """
-    led_yellow.toggle()
+def toggle_led_two():
+    led_two.toggle()
 
 
-def toggle_green():
-    """
-    Allume la LED verte
-    """
-    led_green.toggle()
+def toggle_led_three():
+    led_three.toggle()
 
 
-# Fonctions pour jouer les sons
-def play_red():
-    """
-    Joue le son rouge
-    """
-    # sound_red.play()
+def toggle_led_four():
+    led_four.toggle()
 
 
-def play_yellow():
-    """
-    Joue le son jaune
-    """
-    # sound_yellow.play()
-
-
-def play_green():
-    """
-    Joue le son vert
-    """
-    # sound_green.play()
+button_one.when_pressed = toggle_led_one
+button_two.when_pressed = toggle_led_two
+button_three.when_pressed = toggle_led_three
+button_four.when_pressed = toggle_led_four
 
 
 def get_potentiometer_value():
     """
-    Récupère la valeur du potentiomètre
+    Récupère la valeur du potentiomètre en secondes
     """
-    return potentiometer.value
+    # Transformer la valeur du potentiomètre entre 0 et 1 en secondes
+    # val_in_seconds = potentiometer.value * 10
+    # return round(val_in_seconds)
+    return round(potentiometer.value, 2)
 
 
-# Associer les fonctions aux boutons
-button_red.when_pressed = toggle_red
-button_yellow.when_pressed = toggle_yellow
-button_green.when_pressed = toggle_green
-
-
-def export_sound():
-    """
-    Exporte les sons avec pydub (à tester)
-    """
-    # nb_sounds_to_play = 0
-
-    # Trouver quels leds sont allumées
-    # if led_red.value == 1:
-    #     nb_sounds_to_play += 1
-
-    # if led_yellow.value == 1:
-    #     nb_sounds_to_play += 1
-
-    # if led_green.value == 1:
-    #     nb_sounds_to_play += 1
-
-    # Loader sons avec pydub
-    # sound = AudioSegment.from_wav("kick2.wav")
-    # delay = AudioSegment.silent(duration=get_potentiometer_value() * 1000)
-
-    # sound = None
-    # Combiner les sons avec un delay
-    # for i in range(nb_sounds_to_play):
-    # sound += sound + delay
-
-    # Exporter le son
-    # combined.export("output.wav", format="wav")
-    return True
-
-
-# Boucle infinie pour jouer les sons
 while True:
-    # Si led rouge est allumée, jouer le son rouge
-    if led_red.value == 1:
-        play_red()
-        sleep(get_potentiometer_value())
+    if led_one.value == 1:
+        sound.play()
+        # wt = get_potentiometer_value()
+        # if wt < 0.5:
+        #     sleep(0.5)
+        # else:
+        #     sleep(1)
+        # pygame.time.wait(1000)
+        # sleep(get_potentiometer_value())
+        #         pygame.time.wait(1000)
 
-    # Si led jaune est allumée, jouer le son jaune
-    if led_yellow.value == 1:
-        play_yellow()
-        sleep(get_potentiometer_value())
+    sleep(get_potentiometer_value())
+    # pygame.time.wait(1000)
 
-    # Si led verte est allumée, jouer le son vert
-    if led_green.value == 1:
-        play_green()
-        sleep(get_potentiometer_value())
+    if led_two.value == 1:
+        sound.play()
 
+    sleep(get_potentiometer_value())
+
+    if led_three.value == 1:
+        sound.play()
+    sleep(get_potentiometer_value())
+
+    if led_four.value == 1:
+        sound.play()
+
+    sleep(get_potentiometer_value())
+    print(f"Vitesse : {get_potentiometer_value()}")
     # Attendre 0.1 seconde
     sleep(0.1)
+    # pygame.time.wait(1000)
